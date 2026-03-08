589bf8e601a0c0ca1aea285c9c16cae4b5751234
Artur Shatskov <artur.shackov77@gmail.com>
2026-03-08 17:46:38 +0100

    Fix review comments

--- src/createServer.js
+++ src/createServer.js
@@ -97,7 +97,7 @@ function createServer() {
     const user = users.find((u) => u.id === Number(userId));
 
     if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+      return res.status(400).json({ error: 'User not found' });
     }
 
     const expense = {
